b7747db46ecb675d4eaf22d8d992e619c2ace8de
ghalib77 <muhamadghalib7@gmail.com>
2023-01-15 18:50:40 +0700

    feat: add store model

---
+++ app/Models/Store.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Store extends Model
+{
+    use HasFactory;
+
+    protected $table='store';
+    protected $fillable=[
+        'name',
+        'store_image',
+        'user_id',
+        'location'
+    ];
+    protected $guarded=[
+        'id'
+    ];
+
+    public function user(){
+        return $this->hasOne(User::class, 'user_id', 'id');
+    }
+}
