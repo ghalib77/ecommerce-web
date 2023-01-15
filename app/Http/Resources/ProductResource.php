38c875974339ce6901b0939572f51520de7c4bc0
ghalib77 <muhamadghalib7@gmail.com>
2023-01-15 19:12:38 +0700

    feat: add product resource

---
+++ app/Http/Resources/ProductResource.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+use App\Http\Resources\StoreResource;
+
+class ProductResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            "id"=>$this->id,
+            "name"=>$this->name,
+            "photo_product"=>$this->photo_product,
+            "description"=>$this->description,
+            "quantity"=>$this->quantity,
+            "product_rating"=>$this->product_rating,
+            "store"=>new StoreResource($this->whenLoaded('store')),
+            'created_at' => (new DateTime($this->created_at))->format('Y-m-d H:i:s')
+        ];
+    }
+}
