ea9e712fd59029085adda3233b5c805620b139a7
ghalib77 <muhamadghalib7@gmail.com>
2023-01-16 09:59:41 +0700

    feat: add card migration

---
+++ database/migrations/2023_01_16_022541_card.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Card extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('card', function(Blueprint $table){
+            $table->id();
+            $table->enum('card_type', ['visa', 'mastercard', 'BCA', 'mandiri']);
+            $table->string('card_number')->unique();
+            $table->string('cvv');
+            $table->date('expired_date');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('user')->onDelete('cascade')->onUpdate('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
